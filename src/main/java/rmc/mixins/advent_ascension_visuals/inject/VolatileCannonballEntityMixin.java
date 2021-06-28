c15efe7d6377dd1a0e10d72deb738d6c731c7aec
KR33PY <kr33pyrmc@gmail.com>
2021-06-28 17:08:52 +0300

    1.0.0 - Релиз

---
+++ src/main/java/rmc/mixins/advent_ascension_visuals/inject/VolatileCannonballEntityMixin.java
@@ -0,0 +1,30 @@
+package rmc.mixins.advent_ascension_visuals.inject;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.tslat.aoa3.entity.projectile.cannon.VolatileCannonballEntity;
+
+/**
+ * Developed by RMC Team, 2021
+ * @author KR33PY
+ */
+@Mixin(value = VolatileCannonballEntity.class)
+public abstract class VolatileCannonballEntityMixin {
+
+    private boolean rmc$skip;
+
+    @Inject(method = "Lnet/tslat/aoa3/entity/projectile/cannon/VolatileCannonballEntity;tick()V",
+            cancellable = true,
+            at = @At(value = "INVOKE",
+                     target = "Lnet/tslat/aoa3/util/WorldUtil;createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;F)Lnet/minecraft/world/Explosion;"))
+    private void lowerExplosions(CallbackInfo mixin) {
+        this.rmc$skip = !this.rmc$skip;
+        if (this.rmc$skip) {
+            mixin.cancel();
+        }
+    }
+
+}
